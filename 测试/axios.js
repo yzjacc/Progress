aa199c02824b5392347422d293830532bc8d621c
Monkey <yzjacc@gmail.com>
2020-06-01 14:46:00 +0800

    update

--- 测试/axios.js
+++ 测试/axios.js
@@ -1,7 +1,12 @@
-export default function f() {
-
-	(function (){setInterval(function () {
-		console.log("hahha");
-	},1000)})()
-	console.log("haha11h")
+function NumberOf1(n)
+{
+    // write code here
+    var count = 0,flag=1;
+    while(flag){
+        if(n&flag)count++;
+        flag=flag<<1;
+    }
+    return count;
 }
+
+console.log(NumberOf1(-1));
