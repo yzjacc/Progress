d5f06bfa47cd0fa70d45022793059426f4cb5c64
yzjacc <yzjacc@gmail.com>
2021-01-28 15:48:49 +0800

    feat:整理文件夹

--- manager/lkmanager_api/public/js/base.js
+++
@@ -1,9 +0,0 @@
-// 顶部特效进度条
-NProgress.start();
-NProgress.done();
-
-// 侧边栏伸缩
-$('.navs ul').prev('a').on('click', function () {
-  $(this).next().slideToggle();
-});
-
--- manager/lkmanager_api/src/config.js
+++
@@ -1,8 +0,0 @@
-import {join} from 'path';
-
-export default {
-    viewPath: join(__dirname, '../views'),
-    publicPath: join(__dirname, '../public'),
-    node_modules: join(__dirname, '../node_modules'),
-    upload_path: join(__dirname, '../public/uploads')
-}
