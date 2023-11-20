5cfc769d34eaeeba9e6066d439c7ebed19ee2f2b
yzjacc <yzjacc@gmail.com>
2023-11-20 19:37:34 +0800

    chore: lint

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 // 要扫描的目录路径
-const directoryPath = "./";
+const directoryPath = './';
 
-function walkDir(dirPath, callback) {
-  fs.readdirSync(dirPath).forEach((f) => {
-    let dirFilePath = path.join(dirPath, f);
+// 函数用于递归删除文件夹及其所有内容
+function deleteFolderRecursive(folderPath) {
+    if (fs.existsSync(folderPath)) {
+        fs.readdirSync(folderPath).forEach((file, index) => {
+            const curPath = path.join(folderPath, file);
+            if (fs.lstatSync(curPath).isDirectory()) {
+                // 如果是文件夹，则递归删除
+                deleteFolderRecursive(curPath);
+            } else {
+                // 如果是文件，则删除
+                fs.unlinkSync(curPath);
+            }
+        });
 
-    if (fs.statSync(dirFilePath).isDirectory()) {
-      walkDir(dirFilePath, callback);
-    } else if (path.basename(dirFilePath) === ".gitignore") {
-      callback(path.join(dirPath, f));
+        // 删除文件夹
+        fs.rmdirSync(folderPath);
+        console.log(`Successfully deleted folder at ${folderPath}`);
     }
-  });
 }
 
-walkDir(directoryPath, function (filePath) {
-  fs.unlink(filePath, function (err) {
-    if (err) {
-      console.log("Failed to delete file:", err);
-    } else {
-      console.log(`Successfully deleted file at ${filePath}`);
-    }
-  });
-});
+// 函数用于深度遍历给定的目录，寻找名为 'abc' 的文件夹
+function walkDir(dirPath) {
+    fs.readdirSync(dirPath).forEach(f => {
+        let dirFilePath = path.join(dirPath, f);
+        if (fs.statSync(dirFilePath).isDirectory()) {
+            if (path.basename(dirFilePath) === 'node_modules') {
+                deleteFolderRecursive(dirFilePath);
+            } else {
+                walkDir(dirFilePath);
+            }
+        }
+    });
+};
 
-// Delete all 'abc.txt' files
-deleteFile(directoryPath);
+walkDir(directoryPath);
