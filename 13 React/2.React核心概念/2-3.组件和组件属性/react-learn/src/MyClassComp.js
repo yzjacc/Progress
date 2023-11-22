d4352ad8eba25ae71afe3c3c21cd45116d08eac8
yzjacc <yzjacc@gmail.com>
2023-11-22 17:24:49 +0800

    chore: 顺序更改

---
+++ 13 React/2.React核心概念/2-3.组件和组件属性/react-learn/src/MyClassComp.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default class MyClassComp extends React.Component {
+
+    // 自动完成该阶段
+    // constructor(props) {
+    //     super(props); // this.props = props;
+    //     console.log(props, this.props, props === this.props);
+    // }
+
+    /**
+     * 该方法必须返回React元素
+     */
+    render() {
+        if (this.props.obj) {
+            return (
+                <>
+                    <p>
+                        姓名：{this.props.obj.name}
+                    </p>
+                    <p>
+                        年龄：{this.props.obj.age}
+                    </p>
+                </>
+            );
+        }
+        else if (this.props.ui) {
+            return (
+                <div>
+                    <h1>下面是传入的内容</h1>
+                    {this.props.ui}
+                </div>
+            );
+        }
+        return <h1>类组件的内容，数字：{this.props.number}</h1>
+    }
+}
